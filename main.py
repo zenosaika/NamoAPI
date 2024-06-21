fd25590cfe658cf9c1b86fff81778ff5385cf339
zenosaika <zenosaika@gmail.com>
2024-06-21 21:53:22 +0700

    messaging api

--- main.py
+++ main.py
@@ -1,7 +1,50 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+CHANNEL_ACCESS_TOKEN = '26i4XBXKTjIMN0uJs8NRX9jgxQBMyI7yNrt1ehnk6tE8/MRusauH7e1rOfDuwZ/DES05tX6bOKJ60iclJs8nOSszdm68PziJ2eBJ0kYSnB4xl0bEZh0ER0Ea0asNXq+pZTnKx0fX4265/OIntm2MZAdB04t89/1O/w1cDnyilFU='
+CHANNEL_SECRET = '9d9478e100309e97d672acbe1614cae9'
+
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
+
+@app.get('/hello')
+def hello_word():
+    return {"hello" : "world"}
+
+@app.post('/message')
+async def hello_word(request: Request):
+    signature = request.headers['X-Line-Signature']
+    body = await request.body()
+    
+    try:
+      handler.handle(body.decode('UTF-8'), signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+        if event.message.text == 'สวัสดี' : 
+            sendMessage(event, 'สวัสดีชาวโลก')
+        else:
+            echo(event)
+    
+def echo(event):
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text))
+        
+def sendMessage(event,message):
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=message))
